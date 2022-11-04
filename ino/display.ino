52f2cfa67a094834bc0c12de98b5f6bee8b2334c
Alessandro190320 <Ale.marcheselli27@gmail.com>
2022-11-04 22:15:59 +0100

    created .ino files

---
+++ ino/display.ino
@@ -0,0 +1,11 @@
+// code display
+
+void setup()
+{
+    // put your setup code here, to run once:
+}
+
+void loop()
+{
+    // put your main code here, to run repeatedly:
+}
---
+++ ino/solarPanel.ino
@@ -0,0 +1,11 @@
+// code solar Panel
+
+void setup()
+{
+    // put your setup code here, to run once:
+}
+
+void loop()
+{
+    // put your main code here, to run repeatedly:
+}
