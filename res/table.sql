d8e49390a2792e942eb930ea46212c2c6987cd74
Alessandro190320 <Ale.marcheselli27@gmail.com>
2022-11-04 21:42:26 +0100

    first commit

---
+++ res/table.sql
@@ -0,0 +1,10 @@
+CREATE TABLE arduino ( 
+    id INTEGER UNSIGNED AUTO_INCREMENT,
+    x INTEGER , 
+    feature VARCHAR(255),
+
+    PRIMARY KEY (id) 
+);
+
+INSERT INTO arduino(x,feature) 
+VALUES
