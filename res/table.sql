7a7e0e7214fdd5ad3c271c5c278e989eb886a0e4
Alessandro190320 <Ale.marcheselli27@gmail.com>
2022-11-04 21:43:57 +0100

    create table @nicolasBaratella

--- res/table.sql
+++ res/table.sql
@@ -1,10 +1,15 @@
-CREATE TABLE arduino ( 
+-- codice sql per la creazione della prima tabella.
+CREATE TABLE arduino2223(
     id INTEGER UNSIGNED AUTO_INCREMENT,
-    x INTEGER , 
-    feature VARCHAR(255),
+    feature INTEGER(1),
+    dev_id INTEGER(1),
+    onoff VARCHAR (7),
 
-    PRIMARY KEY (id) 
+    PRIMARY KEY(id)
 );
 
-INSERT INTO arduino(x,feature) 
-VALUES
+-- codice per inserire componente nella tabella creata
+INSERT INTO arduino2223(feature, dev_id, val)
+VALUES(0, /*intercambiabile*/, 'OFF');
+
+-- by @nicolasBaratella
