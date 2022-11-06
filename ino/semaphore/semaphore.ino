152cf8cca99c31a50364b925e881ba0aa776cc36
Alessandro190320 <Ale.marcheselli27@gmail.com>
2022-11-06 16:35:51 +0100

    prototype Semaphore code for @ModenesiGiacomo

--- ino/semaphore/semaphore.ino
+++ ino/semaphore/semaphore.ino
@@ -1,11 +1,35 @@
 // code semaphore
 
+int Rosso = 11;
+int Giallo = 12;
+int Verde = 13;
+int Bot = 3;
+int BotStato = LOW; // parto con il semaforo spento
+int lettura = LOW;
+
 void setup()
 {
-    // put your setup code here, to run once:
+    pinMode(Rosso, OUTPUT);
+    pinMode(Giallo, OUTPUT);
+    pinMode(Verde, OUTPUT);
+    pinMode(Bot, INPUT);
 }
 
 void loop()
 {
-    // put your main code here, to run repeatedly:
+    lettura = digitalRead(Bot);
+
+    if (lettura != BotStato and lettura == HIGH) // se viene rillevata una variazione
+    {
+        // ciclo semaforo acceso
+        digitalWrite(Rosso, HIGH);
+        digitalWrite(Verde, HIGH);
+        digitalWrite(Giallo, HIGH);
+    }
+    else
+    {
+        // lampeggio giallo ( spento )
+    }
+
+    BotStato = lettura; // aggiorno lo stato del bottone
 }
