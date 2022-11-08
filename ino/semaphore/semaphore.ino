5f98986efcf052defa4d12ff48ef08e6dc527391
Tepag <80254508+Tepag@users.noreply.github.com>
2022-11-08 15:44:13 +0100

    Update semaphore.ino
    adattamento dei semafori con la lettura automatica

--- ino/semaphore/semaphore.ino
+++ ino/semaphore/semaphore.ino
@@ -1,4 +1,25 @@
-// code semaphore
+#include <SPI.h>
+#include <HttpClient.h>
+#include <Ethernet.h>
+#include <EthernetClient.h>
+
+// --------------------------- //
+// CAMBIA QUA PER INDIRIZZO IP //
+// --------------------------- //
+const char kHostname[] = "192.168.1.101"; // your pc's ip address
+
+// --------------------------- //
+// CAMBIA QUA PER LE RICHIESTE //
+// --------------------------- //
+const char kPath[] = "/arduini_Open-day/required/semaphore_status"; // php selector page path
+
+byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED }; // mac address
+
+const int kNetworkTimeout = 30*1000; // Number of milliseconds to wait without receiving any data before we give up
+
+const int kNetworkDelay = 5000; // Number of milliseconds to wait if no data is available before trying again
+
+const int stringDim = 20;
 
 int Rosso = 11;
 int Giallo = 12;
@@ -9,39 +30,111 @@ int lettura = LOW;
 
 void setup()
 {
-    pinMode(Rosso, OUTPUT);
-    pinMode(Giallo, OUTPUT);
-    pinMode(Verde, OUTPUT);
-    pinMode(Bot, INPUT);
+  // initialize serial communications at 9600 bps:
+  Serial.begin(9600); 
+
+  pinMode(Rosso, OUTPUT);
+  pinMode(Giallo, OUTPUT);
+  pinMode(Verde, OUTPUT);
+  pinMode(Bot, INPUT);
+
+  while (Ethernet.begin(mac) != 1)
+  {
+    Serial.println("Error getting IP address via DHCP, trying again...");
+    delay(15000);
+  }  
 }
 
 void loop()
 {
-    lettura = digitalRead(Bot);
+  int err =0;
+  char readedString[stringDim];
+  readedString[0]=0;
+  
+  EthernetClient c;
+  HttpClient http(c);
+  
+  err = http.get(kHostname, kPath);
+  
+  if (err == 0)
+  {
+      err = http.skipResponseHeaders();
+      if (err >= 0)
+      {
+        int bodyLen = http.contentLength();
+        
+        // Now we've got to the body, so we can print it out
+        unsigned long timeoutStart = millis();
+        
+        char* readedChar="z";
+        while ( (http.connected() || http.available()) && ((millis() - timeoutStart) < kNetworkTimeout) )
+        {
+            if (http.available())
+            {
+                *readedChar = http.read();
+                // Print out this character
+                strcat(readedString, readedChar);
+                //Serial.print(readedChar);
 
-    if (lettura != BotStato and lettura == HIGH) // se viene rillevata una variazione
-    {
-        // ciclo semaforo acceso
-        digitalWrite(Verde, HIGH);
-        delay(4000);
-        digitalWrite(Verde, LOW);
-        digitalWrite(Giallo, HIGH);
-        delay(1000);
-        digitalWrite(Giallo, LOW);
-        digitalWrite(Rosso, HIGH);
-        delay(4000);
-        digitalWrite(Rosso, LOW);
-    }
-    else
-    {
-        // lampeggio giallo ( spento )
-        digitalWrite(Verde, LOW);
-        digitalWrite(Rosso, LOW);
+                // from char to string
+
+                bodyLen--;
+                // We read something, reset the timeout counter
+                timeoutStart = millis();
+            }
+            else
+            {
+                // We haven't got any data, so let's pause to allow some to
+                // arrive
+                delay(kNetworkDelay);
+            }
+        }
+      }
+      else
+      {
+        Serial.print("Failed to skip response headers: ");
+        Serial.println(err);
+      }
+    
+  }
+  else
+  {
+    //Serial.print("Connect failed: ");
+    //Serial.println(err);
+  }
+  http.stop();
+  c.stop();
+  
+  // ------------------------------------------------------------------------ //
+  // NEL readedString CONTIENE I DATI RICHIESTI, RICORDA DI METTERE LO SPAZIO //
+  // ------------------------------------------------------------------------ //
+  Serial.println(readedString);
+  
+  if (strcmp(readedString, "On")==0){
+      digitalWrite(Verde, HIGH);
+      delay(4000);
+      digitalWrite(Verde, LOW);
+      digitalWrite(Giallo, HIGH);
+      delay(2000);
+      digitalWrite(Giallo, LOW);
+      digitalWrite(Rosso, HIGH);
+      delay(4000);
+      digitalWrite(Rosso, LOW);
+  }
+  else{
+    if(strcmp(readedString, "Off")==0){
+     // lampeggio giallo ( spento )
+      digitalWrite(Verde, LOW);
+      digitalWrite(Rosso, LOW);
+      
+      for(int i=0; i<10; i++){
         digitalWrite(Giallo, HIGH);
         delay(500); // ritardo
         digitalWrite(Giallo, LOW);
         delay(500); // ritardo
+      }
     }
-
-    BotStato = lettura; // aggiorno lo stato del bottone
+  }
+  
+  
 }
