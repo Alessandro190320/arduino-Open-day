c62e10f0180948b75b9b166f00be5ad4eb798968
Tepag <80254508+Tepag@users.noreply.github.com>
2022-11-08 15:13:50 +0100

    Update selector.php

--- required/selector.php
+++ required/selector.php
@@ -66,6 +66,16 @@
         $db->close();   // chiudo la mia connessione
     }
 
+     elseif(isset($_GET["solarPanel_string"])) {
+        $value = $_GET["solarPanel_string"];
+        $db = new mysqli("localhost","root","","scuola2223");   // creazione collegamento con il mio database
+        $sql = "UPDATE arduino SET String = " + $value + " WHERE Descr = \"solarPanel\"";    // creazione della query
+
+        $rs = $db->query($sql);     // result che prende il collegamento e effetua la query
+
+        $db->close();   // chiudo la mia connessione
+    }
+
 
 
 ?>
