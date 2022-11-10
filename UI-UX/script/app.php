b8587ec03043ae8a41261e7c21f35d1aa926a96c
Ghiruz03Lollo <104565083+Ghiruz03Lollo@users.noreply.github.com>
2022-11-10 18:10:14 +0100

    Update app.php

--- UI-UX/script/app.php
+++ UI-UX/script/app.php
@@ -3,9 +3,7 @@
 // ogni volta che si aggiorna lo stato non venga mandato a False di default //
 // ------------------------------------------------------------------------ //
  
-<?php
-    $campo = "0";   //--> Da cambiare, per la riga da leggere.
-?>
+
 // Btn 0
 function on_off_0(type) {
     var btn = document.getElementsByClassName("btn-on-0")[0];
@@ -15,15 +13,15 @@ function on_off_0(type) {
     <?php
         $db = new mysqli("localhost", "root", "", "scuola2223");
 
-        $sql = "SELECT OnOff FROM arduino WHERE descr=$campo";
+        $sql = "SELECT OnOff FROM arduino WHERE descr='semaphore'";
         $rs = $db->query($sql);
         $record = $rs->fetch_assoc();
         $readed = $record['OnOff'];
         if (strcmp($readed, "On")==0){
-            $sql = "UPDATE arduino SET OnOff = 'Off' WHERE descr=$campo";
+            $sql = "UPDATE arduino SET OnOff = 'Off' WHERE descr='semaphore'";
         }
         else{
-            $sql = "UPDATE arduino SET OnOff = 'On' WHERE descr=$campo";
+            $sql = "UPDATE arduino SET OnOff = 'On' WHERE descr='semaphore'";
         }
         $db->query($sql);
         $db->close();
@@ -43,11 +41,10 @@ function on_off_0(type) {
     }
     btn.setAttribute("onclick", "on_off_0(" + !type + ")");
 }
-/*
+
 <?php
-    //$campo = "0";   //--> Da cambiare, per la riga da leggere.
     $db = new mysqli("localhost", "root", "", "scuola2223");
-    $sql = "SELECT OnOff FROM arduino WHERE descr=$campo";
+    $sql = "SELECT OnOff FROM arduino WHERE descr='semaphore'";
     $rs = $db->query($sql);
     $record = $rs->fetch_assoc();
     $readed = $record['OnOff'];
@@ -61,7 +58,6 @@ function on_off_0(type) {
         }
     $db->close();
 ?>
-*/
 
 
 // Btn 1
@@ -70,6 +66,23 @@ function on_off_1(type) {
     var btn = document.getElementsByClassName("btn-on-1")[0];
     var circle = document.getElementsByClassName("btn-on-circle-1")[0];
     var text = document.getElementsByClassName("btn-on-text-1")[0];
+
+    <?php
+        $db = new mysqli("localhost", "root", "", "scuola2223");
+
+        $sql = "SELECT OnOff FROM arduino WHERE descr='house1'";
+        $rs = $db->query($sql);
+        $record = $rs->fetch_assoc();
+        $readed = $record['OnOff'];
+        if (strcmp($readed, "On")==0){
+            $sql = "UPDATE arduino SET OnOff = 'Off' WHERE descr='house1'";
+        }
+        else{
+            $sql = "UPDATE arduino SET OnOff = 'On' WHERE descr='house1'";
+        }
+        $db->query($sql);
+        $db->close();
+    ?>
 
     if (!type) {
         btn.style = "background-color: red;"
@@ -86,6 +99,22 @@ function on_off_1(type) {
     btn.setAttribute("onclick", "on_off_1(" + !type + ")");
 }
 //on_off_1(false)
+<?php
+    $db = new mysqli("localhost", "root", "", "scuola2223");
+    $sql = "SELECT OnOff FROM arduino WHERE descr='house1'";
+    $rs = $db->query($sql);
+    $record = $rs->fetch_assoc();
+    $readed = $record['OnOff'];
+    if (strcmp($readed, "On")==0){
+            echo("on_off_1(false)");
+        }
+        else{
+            if (strcmp($readed, "Off")==0){
+                echo("on_off_1(true)");
+            }
+        }
+    $db->close();
+?>
 
 // Btn 2
 
@@ -93,6 +122,23 @@ function on_off_2(type) {
     var btn = document.getElementsByClassName("btn-on-2")[0];
     var circle = document.getElementsByClassName("btn-on-circle-2")[0];
     var text = document.getElementsByClassName("btn-on-text-2")[0];
+
+    <?php
+        $db = new mysqli("localhost", "root", "", "scuola2223");
+
+        $sql = "SELECT OnOff FROM arduino WHERE descr='house2'";
+        $rs = $db->query($sql);
+        $record = $rs->fetch_assoc();
+        $readed = $record['OnOff'];
+        if (strcmp($readed, "On")==0){
+            $sql = "UPDATE arduino SET OnOff = 'Off' WHERE descr='house2'";
+        }
+        else{
+            $sql = "UPDATE arduino SET OnOff = 'On' WHERE descr='house2'";
+        }
+        $db->query($sql);
+        $db->close();
+    ?>
 
     if (!type) {
         btn.style = "background-color: red;"
@@ -109,6 +155,22 @@ function on_off_2(type) {
     btn.setAttribute("onclick", "on_off_2(" + !type + ")");
 }
 //on_off_2(false)
+<?php
+    $db = new mysqli("localhost", "root", "", "scuola2223");
+    $sql = "SELECT OnOff FROM arduino WHERE descr='house2'";
+    $rs = $db->query($sql);
+    $record = $rs->fetch_assoc();
+    $readed = $record['OnOff'];
+    if (strcmp($readed, "On")==0){
+            echo("on_off_2(false)");
+        }
+        else{
+            if (strcmp($readed, "Off")==0){
+                echo("on_off_2(true)");
+            }
+        }
+    $db->close();
+?>
 
 // Btn 3
 
@@ -116,6 +178,23 @@ function on_off_3(type) {
     var btn = document.getElementsByClassName("btn-on-3")[0];
     var circle = document.getElementsByClassName("btn-on-circle-3")[0];
     var text = document.getElementsByClassName("btn-on-text-3")[0];
+
+    
+    <?php
+        $db = new mysqli("localhost", "root", "", "scuola2223");
+        $sql = "SELECT OnOff FROM arduino WHERE descr='pump'";
+        $rs = $db->query($sql);
+        $record = $rs->fetch_assoc();
+        $readed = $record['OnOff'];
+        if (strcmp($readed, "On")==0){
+            $sql = "UPDATE arduino SET OnOff = 'Off' WHERE descr='pump'";
+        }
+        else{
+            $sql = "UPDATE arduino SET OnOff = 'On' WHERE descr='pump'";
+        }
+        $db->query($sql);
+        $db->close();
+    ?>
 
     if (!type) {
         btn.style = "background-color: red;"
@@ -133,6 +212,23 @@ function on_off_3(type) {
 }
 //on_off_3(false)
 
+<?php
+    $db = new mysqli("localhost", "root", "", "scuola2223");
+    $sql = "SELECT OnOff FROM arduino WHERE descr='pump'";
+    $rs = $db->query($sql);
+    $record = $rs->fetch_assoc();
+    $readed = $record['OnOff'];
+    if (strcmp($readed, "On")==0){
+            echo("on_off_3(false)");
+        }
+        else{
+            if (strcmp($readed, "Off")==0){
+                echo("on_off_3(true)");
+            }
+        }
+    $db->close();
+?>
+
 
 // Btn 4
 
@@ -140,6 +236,23 @@ function on_off_4(type) {
     var btn = document.getElementsByClassName("btn-on-4")[0];
     var circle = document.getElementsByClassName("btn-on-circle-4")[0];
     var text = document.getElementsByClassName("btn-on-text-4")[0];
+
+    <?php
+        $db = new mysqli("localhost", "root", "", "scuola2223");
+
+        $sql = "SELECT OnOff FROM arduino WHERE descr='oled'";
+        $rs = $db->query($sql);
+        $record = $rs->fetch_assoc();
+        $readed = $record['OnOff'];
+        if (strcmp($readed, "On")==0){
+            $sql = "UPDATE arduino SET OnOff = 'Off' WHERE descr='oled'";
+        }
+        else{
+            $sql = "UPDATE arduino SET OnOff = 'On' WHERE descr='oled'";
+        }
+        $db->query($sql);
+        $db->close();
+    ?>
 
     if (!type) {
         btn.style = "background-color: red;"
@@ -156,6 +269,22 @@ function on_off_4(type) {
     btn.setAttribute("onclick", "on_off_4(" + !type + ")");
 }
 //on_off_4(false)
+<?php
+    $db = new mysqli("localhost", "root", "", "scuola2223");
+    $sql = "SELECT OnOff FROM arduino WHERE descr='oled'";
+    $rs = $db->query($sql);
+    $record = $rs->fetch_assoc();
+    $readed = $record['OnOff'];
+    if (strcmp($readed, "On")==0){
+            echo("on_off_4(false)");
+        }
+        else{
+            if (strcmp($readed, "Off")==0){
+                echo("on_off_4(true)");
+            }
+        }
+    $db->close();
+?>
 
 // Btn 5
 
@@ -163,6 +292,23 @@ function on_off_5(type) {
     var btn = document.getElementsByClassName("btn-on-5")[0];
     var circle = document.getElementsByClassName("btn-on-circle-5")[0];
     var text = document.getElementsByClassName("btn-on-text-5")[0];
+
+    <?php
+        $db = new mysqli("localhost", "root", "", "scuola2223");
+
+        $sql = "SELECT OnOff FROM arduino WHERE descr='solarPanel'";
+        $rs = $db->query($sql);
+        $record = $rs->fetch_assoc();
+        $readed = $record['OnOff'];
+        if (strcmp($readed, "On")==0){
+            $sql = "UPDATE arduino SET OnOff = 'Off' WHERE descr='solarPanel'";
+        }
+        else{
+            $sql = "UPDATE arduino SET OnOff = 'On' WHERE descr='solarPanel'";
+        }
+        $db->query($sql);
+        $db->close();
+    ?>
 
     if (!type) {
         btn.style = "background-color: red;"
@@ -179,6 +325,22 @@ function on_off_5(type) {
     btn.setAttribute("onclick", "on_off_5(" + !type + ")");
 }
 //on_off_5(false)
+<?php
+    $db = new mysqli("localhost", "root", "", "scuola2223");
+    $sql = "SELECT OnOff FROM arduino WHERE descr='solarPanel'";
+    $rs = $db->query($sql);
+    $record = $rs->fetch_assoc();
+    $readed = $record['OnOff'];
+    if (strcmp($readed, "On")==0){
+            echo("on_off_5(false)");
+        }
+        else{
+            if (strcmp($readed, "Off")==0){
+                echo("on_off_5(true)");
+            }
+        }
+    $db->close();
+?>
 
 // funzione barra
 /*
