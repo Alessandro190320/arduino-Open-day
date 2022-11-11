e1eea966a4ab2ce4c64700c49d27948a8fca4cac
Tepag <80254508+Tepag@users.noreply.github.com>
2022-11-11 15:36:10 +0100

    Update table.sql
    add led1 and led2 with insert

--- res/table.sql
+++ res/table.sql
@@ -10,13 +10,19 @@ CREATE TABLE arduino(
 
 -- codice per inserire componente nella tabella creata
 INSERT INTO arduino(Id, Descr, OnOff, String)
-VALUES(1, "pump", 'On', ""); -- example of a led 
+VALUES(1, "pump", 'On', "");
 
 INSERT INTO arduino(Id, Descr, OnOff, String)
-VALUES(2, "semaphore", 'On', ""); -- example of a led 
+VALUES(2, "semaphore", 'On', "");
 
 INSERT INTO arduino(Id, Descr, OnOff, String)
-VALUES(3, "solarPanel", 'On', "300"); -- example of a led 
+VALUES(3, "solarPanel", 'On', "300"); 
 
 INSERT INTO arduino(Id, Descr, OnOff, String)
-VALUES(4, "oled", 'On', "sono sul oled"); -- example of a led 
+VALUES(4, "oled", 'On', "sono sul oled"); 
+
+INSERT INTO arduino(Id, Descr, OnOff, String)
+VALUES(5, "led1", 'On', ""); 
+
+INSERT INTO arduino(Id, Descr, OnOff, String)
+VALUES(6, "led2", 'On', ""); 
